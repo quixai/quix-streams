01b5415fcf9884c3c1a053084d0b5b84a5b51b6d
Ales Saska <ales@quix.ai>
2021-10-27 16:43:56 +0200

    fix: azure pipelines

--- .ci/windows/build.cmd
+++ .ci/windows/build.cmd
@@ -1,31 +1,28 @@
-###########################
-## DOWNLOAD DEPENDENCIES ##
-###########################
+:: ###########################
+:: ## DOWNLOAD DEPENDENCIES ##
+:: ###########################
 
 Mkdir lib 
 Mkdir lib\googletest
 git clone https://github.com/google/googletest.git lib/googletest
 
-#################
-##  CONFIGURE  ##
-#################
+:: #################
+:: ##  CONFIGURE  ##
+:: #################
 echo "CONFIGURING"
 
 Mkdir build 
 
 cmake --version
 
-LDFLAGS=-v cmake -S. -Bbuild
-
-
-#############
-##  BUILD  ##
-#############
+:: #############
+:: ##  BUILD  ##
+:: #############
 echo "BUILDING"
 cmake --build build --target all -- VERBOSE=1
 
-############
-##  TEST  ##
-############
+:: ############
+:: ##  TEST  ##
+:: ############
 echo "TESTING"
 cmake --build build --target test
